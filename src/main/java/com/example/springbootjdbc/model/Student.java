5c1776ce4abd23eefbf12741105e61314e22a890
DIR\p.soontornpornlert <p.soontornpornlert@accenture.com>
2021-03-11 14:05:25 +0700

    [EDIT] constructor to lombok annotation

--- src/main/java/com/example/springbootjdbc/model/Student.java
+++ src/main/java/com/example/springbootjdbc/model/Student.java
@@ -1,25 +1,18 @@
 package com.example.springbootjdbc.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
     private long id;
     private String firstName;
     private String lastName;
     private String email;
     private int age;
-
-    public Student() {
-    }
-
-    public Student(long id, String firstName, String lastName, String email, int age) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.age = age;
-    }
 
     public Student(String firstName, String lastName, String email, int age) {
         this.firstName = firstName;
