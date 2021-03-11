78fa35136c9e1bae102a7341887112aa97993a0d
DIR\p.soontornpornlert <p.soontornpornlert@accenture.com>
2021-03-11 13:51:07 +0700

    [EDIT] attribute name

--- src/main/java/com/example/springbootjdbc/exception/StudentException.java
+++ src/main/java/com/example/springbootjdbc/exception/StudentException.java
@@ -2,21 +2,22 @@ package com.example.springbootjdbc.exception;
 
 public class StudentException extends RuntimeException {
 
-    private String message;
+    private String customMessage;
     private int code;
 
-    public StudentException(String message) {
-        super(message);
-        this.message = message;
+    public StudentException(String customMessage) {
+        super(customMessage);
+        this.customMessage = customMessage;
     }
 
-    public StudentException(String message, int code) {
-        super(message);
+    public StudentException(String customMessage, int code) {
+        super(customMessage);
+        this.customMessage = customMessage;
         this.code = code;
     }
 
-    public String getMessage() {
-        return message;
+    public String getCustomMessage() {
+        return customMessage;
     }
 
     public int getCode() {
