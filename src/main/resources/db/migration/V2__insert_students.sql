34a9fa38bd89afbb6210a0b7b241f0185139866e
DIR\p.soontornpornlert <p.soontornpornlert@accenture.com>
2021-03-11 18:07:25 +0700

    [ADD] flyway version 2 (insert some students)

---
+++ src/main/resources/db/migration/V2__insert_students.sql
@@ -0,0 +1,6 @@
+INSERT INTO students (id, first_name, last_name, email, age)
+VALUES (1, 'aaa', 'test', 'aaa@gmail.com', 10),
+       (2, 'bbb', 'test', 'bbb@gmail.com', 20),
+       (3, 'ccc', 'test', 'ccc@gmail.com', 30),
+       (4, 'ddd', 'test', 'ddd@gmail.com', 40),
+       (5, 'eee', 'test', 'eee@gmail.com', 50)
