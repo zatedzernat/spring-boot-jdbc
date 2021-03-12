4179e0ed32771062705ef8ffe3a0acc0b0da394b
DIR\p.soontornpornlert <p.soontornpornlert@accenture.com>
2021-03-12 16:30:53 +0700

    [EDIT] after code review 2

--- src/main/java/com/example/springbootjdbc/model/Student.java
+++ src/main/java/com/example/springbootjdbc/model/Student.java
@@ -3,15 +3,19 @@ package com.example.springbootjdbc.model;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Data
 @Accessors(chain = true)
 public class Student {
     @NotNull
+    @Min(1)
     private long id;
     private String firstName;
     private String lastName;
     private String email;
+    @NotNull
+    @Min(1)
     private int age;
 }
